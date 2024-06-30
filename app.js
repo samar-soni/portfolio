68c90255fce27c98ee09b49d802ec21bf660e1d7
demo <samarsonijee@gmail.com>
2024-06-30 15:41:07 +0530

    HTML and CSS done

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed("#home .details .line3 .skill", {
+        strings: ["Frontend Development","Web Designing", "Backend Development",  "Web Development"],
+        loop: true,
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 500,
+    });
+});
